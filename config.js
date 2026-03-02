69f620f0e044085ffad041830eaedd096214045c
Victor Usen <usenvictor40@gmail.com>
2026-03-02 14:55:57 +0100

    added new SEO codes to enhance search results

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   EMAILJS_PUBLIC_KEY: "qPEL38FQfyjlkzRI4",
   EMAILJS_SERVICE_ID: "service_ykaci9c",
-  EMAILJS_TEMPLATE_ID: "template_hzo7uwj" 
+  EMAILJS_TEMPLATE_ID: "template_hzo7uwj"
 };
