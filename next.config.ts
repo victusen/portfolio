cf99f790ce59e2a2df5078b0af32956cf278279e
Victor Usen <usenvictor40@gmail.com>
2026-05-20 12:51:19 +0100

    images did not load on Netlify live site, next.config.ts need to be tweaked to allow static file exports

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
